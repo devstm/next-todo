cbf33e4e310719b0365177e91a90dd50e9d416f9
IyadIAH <iyadhmidat@gmail.com>
2023-07-23 14:01:28 +0300

    removed localhost

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,32 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Todo() {
   const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
-  const [added, setAdded] = useState('');
+  const [newTodo, setNewTodo] = useState("");
+  const [added, setAdded] = useState("");
 
   useEffect(() => {
     const fetchTodos = async () => {
-      const response = await axios.get('http://localhost:3000/api');
+      const response = await axios.get("/api");
       setTodos(response.data.todos);
     };
     fetchTodos();
   }, [added]);
 
   const addTodo = async () => {
-    const response = await axios.post('http://localhost:3000/api', { text: newTodo });
+    const response = await axios.post("/api", { text: newTodo });
     setTodos([...todos, response.data]);
-    setAdded(response.data)
-    setNewTodo('');
+    setAdded(response.data);
+    setNewTodo("");
   };
 
   return (
     <div className="App">
-      <input value={newTodo} onChange={e => setNewTodo(e.target.value)} />
+      <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
       <button onClick={addTodo}>Add</button>
-      {todos?.map(todo => (
+      {todos?.map((todo) => (
         <div key={todo._id}>{todo.text}</div>
       ))}
     </div>
